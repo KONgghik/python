483fd77924c5481f90e36d98b97b490c494ba55b
KONgghik <111270736+KONgghik@users.noreply.github.com>
2023-09-25 12:48:55 +0700

    Update Bmki-python.py

--- Bmki-python.py
+++ Bmki-python.py
@@ -1,2 +1,17 @@
-from pystyle import *
-print("ลองดูไม่หน้าเสียหาย")
+import requests
+
+# ดึงคีย์จาก URL
+r = requests.get("https://raw.githubusercontent.com/KONgghik/python/main/key.txt")
+key = r.text.strip()  # เอาอักษรว่างที่อาจมีอยู่ด้วย
+
+# รับคีย์จากผู้ใช้
+user_input = input("key: ")
+
+# เปรียบเทียบคีย์
+if user_input == key:
+    print("คีย์ถูกต้อง")
+else:
+    print("คีย์ไม่ถูกต้อง")
+    exit()
+
+print("ตัวนี้ขั้นทดลองรออัพเดท")
